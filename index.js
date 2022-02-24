27e7afabf24d296607a05374e2565d2ebfba5796
Nadira Ali <nadiraali188a@gmail.com>
2022-02-24 14:04:50 +0000

    woking script with pseudo-code for future steps

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+/*
+Step 1: Set up inquirer function.
+Step 2: Produce array of questions for users.
+Step 3: Generate answers from users.
+Step 4: Write responses to files
+*/
+
+const inquirer = require('inquirer');
+
+async function generateReadMe(){
+    console.log('hello world')
+} 
+
+generateReadMe()
